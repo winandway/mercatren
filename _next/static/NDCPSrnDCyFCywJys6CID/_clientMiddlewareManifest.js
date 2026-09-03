fded8240a1cbb9f8d119cee4a33b4028bf9923d3
winandway <winandway@users.noreply.github.com>
2026-09-03 01:33:30 +0000

    deploy: 9311b228398b2c245fbd85ba36a292c28d9a7cb2

--- _next/static/NDCPSrnDCyFCywJys6CID/_clientMiddlewareManifest.js
+++
@@ -1,6 +0,0 @@
-self.__MIDDLEWARE_MATCHERS = [
-  {
-    "regexp": "^(?:\\/(_next\\/data\\/[^/]{1,}))?(?:\\/((?!_next|datos|media|upload|__scheduled|sw.js|manifest.json|.*\\..*).*))(\\.json|\\.rsc|\\.segments\\/.+\\.segment\\.rsc)?[\\/#\\?]?$",
-    "originalSource": "/((?!_next|datos|media|upload|__scheduled|sw.js|manifest.json|.*\\..*).*)"
-  }
-];self.__MIDDLEWARE_MATCHERS_CB && self.__MIDDLEWARE_MATCHERS_CB()
---
+++ _next/static/NInft-osXdkI66hLiRZRA/_clientMiddlewareManifest.js
@@ -0,0 +1,6 @@
+self.__MIDDLEWARE_MATCHERS = [
+  {
+    "regexp": "^(?:\\/(_next\\/data\\/[^/]{1,}))?(?:\\/((?!_next|datos|media|upload|sw.js|manifest.json|.*\\..*).*))(\\.json|\\.rsc|\\.segments\\/.+\\.segment\\.rsc)?[\\/#\\?]?$",
+    "originalSource": "/((?!_next|datos|media|upload|sw.js|manifest.json|.*\\..*).*)"
+  }
+];self.__MIDDLEWARE_MATCHERS_CB && self.__MIDDLEWARE_MATCHERS_CB()
